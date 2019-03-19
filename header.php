78efcfd5a3918223d07886b7bbce9683d1eb8808
Jason Halsey <jason@halsey-design.com>
2019-03-18 22:02:32 -0700

    Add menu active state

--- header.php
+++ header.php
@@ -31,12 +31,12 @@
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'underscore_2_14_19' ); ?></button>
 			<?php
  
-if ( is_active_sidebar( 'custom-header-widget' ) ) : ?>
-    <div id="header-widget-area" class="chw-widget-area widget-area" role="complementary">
-    <?php dynamic_sidebar( 'custom-header-widget' ); ?>
-    </div>
-     
-<?php endif; ?>
+				if ( is_active_sidebar( 'custom-header-widget' ) ) : ?>
+				    <div id="header-widget-area" class="chw-widget-area widget-area" role="complementary">
+				    <?php dynamic_sidebar( 'custom-header-widget' ); ?>
+				    </div>
+				     
+			<?php endif; ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
